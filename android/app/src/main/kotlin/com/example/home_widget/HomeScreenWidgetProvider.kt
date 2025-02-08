ede62af4dd4b51a41a86dc8f1a1a75a2c935a532
Priyansh Gupta <priyansh.dev02@gmail.com>
2025-02-08 13:19:02 +0530

    Fix the minor changes

--- android/app/src/main/kotlin/com/example/home_widget/HomeScreenWidgetProvider.kt
+++ android/app/src/main/kotlin/com/example/home_widget/HomeScreenWidgetProvider.kt
@@ -16,15 +16,12 @@ class HomeScreenWidgetProvider : AppWidgetProvider() {
         appWidgetIds: IntArray
     ) {
         for (appWidgetId in appWidgetIds) {
-            // Get data
             val widgetData = HomeWidgetPlugin.getData(context)
             val counter = widgetData.getInt("counter", 0)
 
-            // Create widget layout
             val views = RemoteViews(context.packageName, R.layout.widget_layout).apply {
                 setTextViewText(R.id.widget_counter_text, counter.toString())
                 
-                // Create intent for button clicks
                 val intent = Intent(context, HomeScreenWidgetProvider::class.java).apply {
                     action = AppWidgetManager.ACTION_APPWIDGET_UPDATE
                     putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds)
@@ -37,11 +34,9 @@ class HomeScreenWidgetProvider : AppWidgetProvider() {
                     PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
                 )
                 
-                // Set click listener on button
                 setOnClickPendingIntent(R.id.widget_increment_button, pendingIntent)
             }
 
-            // Update widget
             appWidgetManager.updateAppWidget(appWidgetId, views)
         }
     }
@@ -50,18 +45,14 @@ class HomeScreenWidgetProvider : AppWidgetProvider() {
         super.onReceive(context, intent)
         
         if (context != null && intent?.action == AppWidgetManager.ACTION_APPWIDGET_UPDATE) {
-            // Get current counter value
             val widgetData = HomeWidgetPlugin.getData(context)
             var counter = widgetData.getInt("counter", 0)
             
-            // Increment counter
             counter++
             
-            // Save new value using the shared preferences editor
             val prefs = HomeWidgetPlugin.getData(context)
             prefs.edit().putInt("counter", counter).apply()
             
-            // Update all widgets
             val appWidgetManager = AppWidgetManager.getInstance(context)
             val appWidgetIds = intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS)
             if (appWidgetIds != null) {
--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Home Widget Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const MyHomePage(title: 'Flutter Home Widget Demo'),
